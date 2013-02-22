99367fa5a502ddd3348deeb4b066232af3e44780
Aleksandar Gavrilović <aleksandar.gavrilovic@fer.hr>
2013-02-22 01:09:43 +0100

    twitters CSV package compatibility

---
+++ src/importCsvToFile.py
@@ -0,0 +1,39 @@
+﻿#!/usr/bin/python
+
+import csv
+import sys
+import twitterDB
+
+class Status:
+    def __init__(self, **entries): 
+        self.__dict__.update(entries)
+
+        
+def csvToDictionary(csvFilename):
+    csvFile = open(csvFilename,'rb')
+    tweetReader = csv.reader(csvFile, delimiter=',', quotechar='\"')
+    firstRow = tweetReader.next()
+    tweets = []
+    for row in tweetReader:
+        myDictionary = dict(zip(firstRow, row))
+        tweets.append(myDictionary)
+    return tweets
+
+def csvToObject(csvFilename):
+    csvFile = open(csvFilename,'rb')
+    tweetReader = csv.reader(csvFile, delimiter=',', quotechar='\"')
+    firstRow = tweetReader.next()
+    tweets = []
+    for row in tweetReader:
+        myDictionary = dict(zip(firstRow, row))
+        tweet = Status(**myDictionary)
+        tweets.append(tweet)
+    return tweets
+        
+
+if __name__ == "__main__":
+    try:
+        csvFilename = sys.argv[1]
+    except IndexError:
+        csvFilename = "tweets.csv"
+    print csvToDictionary(csvFilename)
